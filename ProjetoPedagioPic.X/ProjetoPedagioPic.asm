fdf9f96488bac2f7063b1d98e3ca129e35f8762f
Joao F P Souza <jfpadilha@gmail.com>
2015-11-27 06:32:10 -0200

    versão v3.0

--- ProjetoPedagioPic.X/ProjetoPedagioPic.asm
+++ ProjetoPedagioPic.X/ProjetoPedagioPic.asm
@@ -34,10 +34,8 @@
  endc
 
  org 0
-    BANCO1
-    call inicia_lcd
-    call msg_bem_vindo
-    
+ 
+        
  ;Inicializacoes
     movlw 0
     movwf valor_entrada
@@ -48,20 +46,20 @@
     movwf qtd_troco  
  
  ;DEFINIR SAIDAS
-    movlw 0
+    BANCO1
+;    movlw 0
+;    movwf TRISD             ;saida para funcionar LCD
+    movlw b'11100000'
     movwf TRISB             ; porta B é saída 
-    movlw b'11101100'       ; PSMODE = 0 para porta D ser I/O
+    movlw b'11100000'       ; PSMODE = 0 para porta D ser I/O
     movwf TRISE             ; bits 0 e 1 da porta E são saídas 
-    movlw b'11101100'       ; pinos configurados como digitais
+    movlw b'00000000'       ; pinos configurados como digitais
     movwf TRISC             ; bits 0 e 1 da porta E são saídas 
-    movlw b'11101100'       ; pinos configurados como digitais
     
 ;DEFINIR ENTRADAS
-    movlw 1
-    movlw TRISD
     movlw b'11111111'
-    movlw TRISA
-    movlw b'11111111'
+    movwf TRISA
+    movlw b'00000100'
     movwf ADCON1
 
 ;CONFIGURACAO PRESCALER
@@ -79,14 +77,10 @@
     bcf sm
     bcf la
    
-inicio                          ;ver se tem erro
-    ;tag vq1
-  
-;configuracao pinos
-ler_entrada_analogica
+inicio
     BANCO1
-    movwf 1
-    movwf TRISD
+    movwf b'00000011'
+    movwf TRISD             ;D como ENTRADA
     movlw b'00000011'       ;pinos configurados para analogico
     movwf ADCON1   
     BANCO0
@@ -101,40 +95,98 @@ testa_ad
     
     movlw peso_veiculo
     sublw 195
-    movwf valor_teste
+    ;movwf valor_teste
     
     btfsc STATUS, C               ;se for zero, pula
-    goto identifica_veiculo         ;não
-    goto ler_entrada_analogica      ;volta
-
+    goto inicio                ;volta se for zero
+    goto identifica_veiculo       ;se nao for zero
+                    
 identifica_veiculo
-    ;testa caminhao 4 eixos
-    goto abrir_cancela
+    BANCO1
+    movwf b'00000000'
+    movwf TRISD             ;D como SAIDA
     
-;    movlw peso_veiculo
-;    sublw 225
-;    movwf valor_teste    
-;    btfsc STATUS, C               ;se for zero, pula
-;;    goto
+    BANCO0
+    movlw peso_veiculo
+    sublw 225
+    btfsc STATUS, C
+    goto seta_valor10             ;se for zero
+    goto identifica_truck         ;se nao for zero        
     
+identifica_truck
+    movlw peso_veiculo
+    sublw 221
+    btfsc STATUS, C
+    goto seta_valor7              ;se for zero
+    goto identifica_carro         ;se nao for zero
+       
+identifica_carro
+    movlw peso_veiculo
+    sublw 209
+    btfsc STATUS, C    
+    goto seta_valor5              ;se for zero
+    goto identifica_moto          ;se nao for zero
+    
+identifica_moto
+    goto abrir_cancela            ;se for zero
+
 seta_valor10
+    call limpa_lcd
+    call inicia_lcd
+    call valor_10
+    movlw 10
+    movwf valor_veiculo
+    goto ler_valor_entrada
     
 seta_valor7
+    call limpa_lcd
+    call inicia_lcd
+    call valor_7
+    movlw 7
+    movwf valor_veiculo
+    goto ler_valor_entrada
     
 seta_valor5
-    
+    call limpa_lcd
+    call inicia_lcd
+    call valor_5
+    movlw 5
+    movwf valor_veiculo
+    goto ler_valor_entrada
     
 ler_valor_entrada
     btfsc n2
-    goto ler_valor_entrada  ;se eh zero
+    goto valor_entrada_2
+    goto ler_n5
     
-ler_n5                  ;sim
-    btfsc n2
-    goto ler_valor_entrada
-
+ler_n5                  
+    btfsc n5
+    goto valor_entrada_5
+    goto ler_valor_entrada    
  
- goto $
+valor_entrada_2
+    movlw 2
+    movwf valor_entrada
+    call espera_2s 
+    goto verifica_valor_entrada
+    
+valor_entrada_5
+    movlw 5
+    movwf valor_entrada
+    call espera_2s 
+    goto verifica_valor_entrada
  
+verifica_valor_entrada
+    movlw valor_entrada
+    addlw valor_salvo
+    sublw valor_veiculo
+    movwf valor_restante
+    movlw 0
+    movwf valor_entrada
+    ;btfsc STATUS, valor_restante
+    ;goto ....				    ; =1 
+    goto abrir_cancela			    ; =0 ele vai para abrir cancela 
+   
 valor_isento
     call inicia_lcd
     movlw 'M'
@@ -231,6 +283,12 @@ valor_10
 
 msg_bem_vindo
     call inicia_lcd
+    movlw ' '
+    call escreve_dado_lcd
+    movlw ' '
+    call escreve_dado_lcd
+    movlw ' '
+    call escreve_dado_lcd
     movlw 'B'
     call escreve_dado_lcd
     movlw 'E'
@@ -325,20 +383,54 @@ msg_cancela_aberta
  
  ;------ 1 segundo------
 espera_1s
-    movlw 20
-    movwf contador
-    movlw 60		; valor para 196 contagens (50ms)
-    movwf TMR0		; 256  -  196  = 60
+ movlw 20
+ movwf contador
+ movlw 60		; valor para 196 contagens (50ms)
+ movwf TMR0		; 256  -  196  = 60
 
 aguarda_estouro 
-    btfss INTCON, TMR0IF	; espera timer0 estourar
-    goto aguarda_estouro
-    movlw 60		; reprograma para 196 contagens (50ms)
-    movwf TMR0		; 256  -  196  = 60
-    bcf INTCON, TMR0IF	; limpa flag de estouro
-    decfsz contador	; aguarda 20 ocorrencias ( 20 x 50ms = 1s)
-    goto aguarda_estouro
-    return
+ btfss INTCON, TMR0IF	; espera timer0 estourar
+ goto aguarda_estouro
+ movlw 60		; reprograma para 196 contagens (50ms)
+ movwf TMR0		; 256  -  196  = 60
+ bcf INTCON, TMR0IF	; limpa flag de estouro
+ decfsz contador	; aguarda 20 ocorrencias ( 20 x 50ms = 1s)
+ goto aguarda_estouro
+ return
+ 
+  ;------ 2 segundo pisca led------
+espera_2s
+ movlw 40
+ movwf contador
+ movlw 60		; valor para 196 contagens (50ms)
+ movwf TMR0		; 256  -  196  = 60
+
+aguarda_estouro_2s 
+ btfss INTCON, TMR0IF	; espera timer0 estourar
+ goto aguarda_estouro_2s
+ movlw 60		; reprograma para 196 contagens (50ms)
+ movwf TMR0		; 256  -  196  = 60
+ bcf INTCON, TMR0IF	; limpa flag de estouro
+ decfsz contador	; aguarda 20 ocorrencias ( 20 x 50ms = 1s)
+ goto aguarda_estouro_2s
+ return
+ 
+  ;------ 4 segundo ------
+espera_4s
+ movlw 80
+ movwf contador
+ movlw 60		; valor para 196 contagens (50ms)
+ movwf TMR0		; 256  -  196  = 60
+
+aguarda_estouro_4s 
+ btfss INTCON, TMR0IF	; espera timer0 estourar
+ goto aguarda_estouro_4s
+ movlw 60		; reprograma para 196 contagens (50ms)
+ movwf TMR0		; 256  -  196  = 60
+ bcf INTCON, TMR0IF	; limpa flag de estouro
+ decfsz contador	; aguarda 20 ocorrencias ( 20 x 50ms = 1s)
+ goto aguarda_estouro_4s
+ return
  
  ;------------| PROCESSAMENTO DE TROCO |------------
 
@@ -351,7 +443,9 @@ devolve_moedas
     call espera_1s
     bcf lm
     decfsz valor_restante
+    goto devolve_moedas
     call limpa_lcd
+    call inicia_lcd
     call msg_cancela_aberta
     goto abrir_cancela
  
@@ -361,9 +455,7 @@ abrir_cancela
     call limpa_lcd
     call inicia_lcd
     call msg_cancela_aberta
-    call espera_1s
-    call espera_1s
-    call espera_1s
+    call espera_4s
 	
 fechar_cancela
     bcf ac
